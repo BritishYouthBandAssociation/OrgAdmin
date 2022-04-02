bc569819e2ddb4e12aa0c417a8e8b575801fc140
Luke <rugulous1999@hotmail.com>
2022-04-02 18:26:05 +0100

    Membership layout tweaks

--- routes/membership.js
+++ routes/membership.js
@@ -6,7 +6,36 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	return res.render('membership/index.hbs', {
-		title: "Membership"
+		title: "Membership",
+		membership: [
+			{
+				name: 'Revolution',
+				number: 'GOL0001',
+				type: {
+					id: 1,
+					name: "Band"
+				},
+				labels: ['Northern']
+			},
+			{
+				name: 'Phantom Knights',
+				number: 'SIL0001',
+				type: {
+					id: 1,
+					name: "Band"
+				},
+				labels: ['Midlands']
+			},
+			{
+				name: 'Luke Taylor',
+				number: 'IND0001',
+				type: {
+					id: 2,
+					name: "Individual"
+				},
+				labels: []
+			}
+		]
 	});
 });
 
