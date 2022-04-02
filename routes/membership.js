cc97a6e42d8cfc7f79f9eccfe1cd291c7fc16423
Luke <rugulous1999@hotmail.com>
2022-04-02 16:23:55 +0100

    Added membership route

---
+++ routes/membership.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Import modules
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	return res.render('membership/index.hbs', {
+		title: "Membership"
+	});
+});
+
+module.exports = {
+	root: '/membership/',
+	router: router
+}
