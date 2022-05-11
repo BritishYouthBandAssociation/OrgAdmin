7ffac0d3d557d3483aa5daed800a55d6f3d4e6ad
Luke <rugulous1999@hotmail.com>
2022-05-11 19:08:37 +0100

    Display existing users

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,22 @@
 'use strict';
 
+/* global __lib */
+
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const {
+	repositories: {
+		UserRepository
+	}
+} = require(__lib);
+
+router.get('/', async (req, res) => {
+	const users = await UserRepository.getAll(req.db);
+
 	return res.render('user/index.hbs', {
-		title: 'Users'
+		title: 'Users',
+		users: users
 	});
 });
 
