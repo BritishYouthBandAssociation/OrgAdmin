36c12b29b0ecb95157bcc30171d18a3bd640e0ef
Luke <rugulous1999@hotmail.com>
2022-06-15 23:24:16 +0100

    Copy to org type

--- routes/config.js
+++ routes/config.js
@@ -47,6 +47,40 @@ router.post('/membership-type', async (req, res) => {
 	return res.redirect("?saved=1");
 });
 
+router.get('/organisation-type', async (req, res) => {
+	const types = await req.db.OrganisationType.findAll();
+
+	return res.render('config/organisation-type.hbs', {
+		title: 'Organisation Types',
+		types: types,
+		saved: req.query.saved ?? false
+	});
+});
+
+router.post('/organisation-type', async (req, res) => {
+	for (let i = 0; i < req.body.type.length; i++){
+		if (req.body.id[i] < 0){
+			//insert
+			await req.db.OrganisationType.create({
+				Name: req.body.type[i],
+				IsActive: req.body.isActive[i]
+			});
+		} else {
+			//update
+			await req.db.MembershipType.update({
+				Name: req.body.type[i],
+				IsActive: req.body.isActive[i]
+			}, {
+				where: {
+					id: req.body.id[i]
+				}
+			});
+		}
+	}
+
+	return res.redirect("?saved=1");
+});
+
 module.exports = {
 	root: '/config/',
 	router: router
