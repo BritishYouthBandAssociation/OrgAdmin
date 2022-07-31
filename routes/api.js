b7c890d29e272776ea4bb2890be0a89fc4c84e4e
Luke <rugulous1999@hotmail.com>
2022-07-31 18:49:52 +0100

    Allow filtering by membership type (/season)

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,30 @@ const router = express.Router();
 
 router.get('/organisation', async (req, res) => {
 	const where = {};
+	const include = new Set();
 
-	if (req.query.type != null){
+	if (req.query.type != null) {
 		where.OrganisationTypeID = req.query.type;
+		include.add(req.db.OrganisationType);
+	}
+
+	if (req.query.membership != null) {
+		include.add({
+			model: req.db.OrganisationMembership,
+			include: [{
+				model: req.db.Membership,
+				where: {
+					MembershipTypeId: req.query.membership,
+					Season: req.query.season ?? 2022 //TODO unhardcode
+				},
+				required: true
+			}],
+			required: true
+		});
 	}
 
 	const orgs = await req.db.Organisation.findAll({
-		include: [ req.db.OrganisationType ],
+		include: include.size > 0 ? Array.from(include) : null,
 		where: where
 	});
 
