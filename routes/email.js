764cd7da9a0f6aeab6c2111fe20633156b629bd2
Luke <rugulous1999@hotmail.com>
2022-08-08 16:02:36 +0100

    Send real email

--- routes/email.js
+++ routes/email.js
@@ -17,7 +17,8 @@ router.get('/', async (req, res) => {
 	return res.render('email/index.hbs', {
 		title: 'Bulk Emailer',
 		types: types,
-		season: 2022
+		season: 2022,
+		success: req.query.success ?? false
 	});
 });
 
@@ -61,43 +62,65 @@ router.post('/test', async (req, res) => {
 	});
 });
 
-router.post('/send', (req, res) => {
+router.post('/send', async (req, res) => {
 	if (!req.session.user.IsAdmin) {
 		return res.redirect("/no-access");
 	}
 
-	//let recipients = [];
+	const t = await req.db.sequelize.transaction();
 
-	// if (req.query.test) {
-	// 	recipients = [{
-	// 		Name: `${req.session.user.FirstName} ${req.session.user.Surname}`,
-	// 		EmailAddress: req.session.user.Email,
-	// 		MessageId: msg.id
-	// 	}];
-	// } else {
-	// 	recipients = (await Promise.all(req.body.membership.map(async m => {
-	// 		const member = await req.db.Membership.findByPk(m, {
-	// 			include: {
-	// 				all: true,
-	// 				nested: true
-	// 			}
-	// 		});
+	try {
+		await Promise.all(req.body.membership.map(async m => {
+			const member = await req.db.Membership.findByPk(m, {
+				include: {
+					all: true,
+					nested: true
+				}
+			});
 
-	// 		const contacts = member.OrganisationMembership?.Organisation?.OrganisationUsers ?? [member.IndividualMembership];
+			const contacts = member.OrganisationMembership?.Organisation?.OrganisationUsers
+				?? [member.IndividualMembership];
 
-	// 		return contacts.map(c => {
-	// 			//console.log(c.dataValues);
+			return Promise.all(contacts.map(async c => {
+				const msg = await req.db.Message.create({
+					Sender: req.body.sender,
+					Subject: req.body.subject,
+					Body: req.body.message.replaceAll('${firstName}', c.User.FirstName).replaceAll('${lastName}', c.User.Surname).replaceAll('${bandName}', member.OrganisationMembership?.Organisation?.Name ?? '')
+				}, {
+					transaction: t
+				});
 
-	// 			return {
-	// 				Name: `${c.User.FirstName} ${c.User.Surname}`,
-	// 				EmailAddress: c.User.Email,
-	// 				MessageId: msg.id
-	// 			}
-	// 		});
-	// 	}))).flat();
-	// }
+				await req.db.Recipient.create({
+					Name: `${c.User.FirstName} ${c.User.Surname}`,
+					EmailAddress: c.User.Email,
+					MessageId: msg.id
+				}, {
+					transaction: t
+				});
+			}));
+		}));
 
-	return res.redirect('./');
+		t.commit();
+
+		res.redirect('./?success=1');
+	} catch (ex) {
+		t.rollback();
+
+		const types = await req.db.MembershipType.findAll({
+			where: {
+				IsActive: true
+			}
+		});
+
+		res.render('email/index.hbs', {
+			title: 'Bulk Emailer',
+			types: types,
+			season: 2022,
+			subject: req.body.subject,
+			message: req.body.message,
+			error: true
+		});
+	}
 });
 
 module.exports = {
