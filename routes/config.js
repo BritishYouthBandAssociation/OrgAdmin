1bf75e843ba8aea877f36db8fd3e3ebe06f3a5c8
Luke <rugulous1999@hotmail.com>
2022-08-11 12:49:52 +0100

    Save division

--- routes/config.js
+++ routes/config.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 const checkAdmin = (req, res, next) => {
-	if (!req.session.user.IsAdmin){
+	if (!req.session.user.IsAdmin) {
 		return res.redirect("/no-access");
 	}
 
@@ -35,49 +35,41 @@ router.get('/membership-type', checkAdmin, async (req, res) => {
 });
 
 router.post('/membership-type', checkAdmin, async (req, res) => {
-	for (let i = 0; i < req.body.type.length; i++){
+	for (let i = 0; i < req.body.type.length; i++) {
 		let labelID = req.body.lbl[i];
 
-		if (labelID < 0){
-			//insert
-			const lbl = await req.db.Label.create({
-				Name: req.body.type[i],
-				BackgroundColour: req.body.bg[i],
-				ForegroundColour: req.body.fg[i]
-			});
+		let details = {
+			Name: req.body.type[i],
+			BackgroundColour: req.body.bg[i],
+			ForegroundColour: req.body.fg[i]
+		};
 
+		if (labelID < 0) {
+			//insert
+			const lbl = await req.db.Label.create(details);
 			labelID = lbl.id;
 		} else {
 			//update
-			await req.db.Label.update({
-				Name: req.body.type[i],
-				BackgroundColour: req.body.bg[i],
-				ForegroundColour: req.body.fg[i]
-			}, {
+			await req.db.Label.update(details, {
 				where: {
 					id: labelID
 				}
 			});
 		}
 
-		if (req.body.id[i] < 0){
-			//insert
-			await req.db.MembershipType.create({
-				Name: req.body.type[i],
-				IsActive: req.body.isActive[i],
-				IsOrganisation: req.body.isOrganisation[i],
-				Cost: req.body.cost[i],
-				LabelId: labelID
-			});
+		details = {
+			Name: req.body.type[i],
+			IsActive: req.body.isActive[i],
+			IsOrganisation: req.body.isOrganisation[i],
+			Cost: req.body.cost[i],
+			LabelId: labelID
+		};
+
+		if (req.body.id[i] < 0) {
+			await req.db.MembershipType.create(details);
 		} else {
 			//update
-			await req.db.MembershipType.update({
-				Name: req.body.type[i],
-				IsActive: req.body.isActive[i],
-				IsOrganisation: req.body.isOrganisation[i],
-				Cost: req.body.cost[i],
-				LabelId: labelID
-			}, {
+			await req.db.MembershipType.update(details, {
 				where: {
 					id: req.body.id[i]
 				}
@@ -99,25 +91,22 @@ router.get('/organisation-type', checkAdmin, async (req, res) => {
 });
 
 router.post('/organisation-type', checkAdmin, async (req, res) => {
-	for (let i = 0; i < req.body.type.length; i++){
-		if (req.body.id[i] < 0){
-			//insert
-			await req.db.OrganisationType.create({
-				Name: req.body.type[i],
-				IsActive: req.body.isActive[i]
-			});
-		} else {
-			//update
-			await req.db.OrganisationType.update({
-				Name: req.body.type[i],
-				IsActive: req.body.isActive[i]
-			}, {
-				where: {
-					id: req.body.id[i]
-				}
-			});
+	await Promise.all(req.body.type.map((t, i) => {
+		const details = {
+			Name: req.body.type[i],
+			IsActive: req.body.isActive[i]
+		};
+
+		if (req.body.id[i] < 0) {
+			return req.db.OrganisationType.create(details);
 		}
-	}
+
+		return req.db.OrganisationType.update(details, {
+			where: {
+				id: req.body.id[i]
+			}
+		});
+	}));
 
 	return res.redirect("?saved=1");
 });
@@ -134,17 +123,16 @@ router.get('/event-type', checkAdmin, async (req, res) => {
 
 router.post('/event-type', checkAdmin, async (req, res) => {
 	await Promise.all(req.body.type.map((type, i) => {
-		if (req.body.id[i] < 0) {
-			return req.db.EventType.create({
-				Name: req.body.type[i],
-				IsActive: req.body.isActive[i]
-			});
-		}
-
-		return req.db.EventType.update({
+		const details = {
 			Name: req.body.type[i],
 			IsActive: req.body.isActive[i]
-		}, {
+		};
+
+		if (req.body.id[i] < 0) {
+			return req.db.EventType.create(details);
+		}
+
+		return req.db.EventType.update(details, {
 			where: {
 				id: req.body.id[i]
 			}
@@ -166,17 +154,16 @@ router.get('/payment-type', checkAdmin, async (req, res) => {
 
 router.post('/payment-type', checkAdmin, async (req, res) => {
 	await Promise.all(req.body.type.map((type, i) => {
-		if (req.body.id[i] < 0) {
-			return req.db.PaymentType.create({
-				Description: req.body.type[i],
-				IsActive: req.body.isActive[i]
-			});
-		}
-
-		return req.db.PaymentType.update({
+		const details = {
 			Description: req.body.type[i],
 			IsActive: req.body.isActive[i]
-		}, {
+		};
+
+		if (req.body.id[i] < 0) {
+			return req.db.PaymentType.create(details);
+		}
+
+		return req.db.PaymentType.update(details, {
 			where: {
 				id: req.body.id[i]
 			}
@@ -191,8 +178,30 @@ router.get('/division', checkAdmin, async (req, res) => {
 
 	return res.render('config/division.hbs', {
 		title: 'Divisions',
-		divisions: divisions
+		divisions: divisions,
+		saved: req.query.saved ?? false
 	});
+});
+
+router.post('/division', checkAdmin, async (req, res) => {
+	await Promise.all(req.body.division.map((d, i) => {
+		const details = {
+			Name: req.body.division[i],
+			IsActive: req.body.isActive[i]
+		};
+
+		if (req.body.id[i] < 0) {
+			return req.db.Division.create(details);
+		}
+
+		return req.db.Division.update(details, {
+			where: {
+				id: req.body.id[i]
+			}
+		});
+	}));
+
+	return res.redirect('?saved=true');
 });
 
 module.exports = {
