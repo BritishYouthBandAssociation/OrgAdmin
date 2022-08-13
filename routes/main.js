30a9b2f1befc5636099cca788fbf10ad7b11110c
matt <matt@fiddaman.net>
2022-08-13 17:19:48 +0000

    Convert main route

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,14 @@
 'use strict';
 
-/* global __lib */
-
 // Import modules
 const express = require('express');
 const router = express.Router();
-const lib = require(__lib);
+const Joi = require('joi');
 
-// Set up default route to check server is running
+const validator = require('@byba/express-validator');
+
 router.get('/', (req, res, next) => {
-	//single equals to include undefined
-	if (req.session.user != null) {
+	if (req.session.user) {
 		return res.redirect('home');
 	}
 
@@ -20,39 +18,37 @@ router.get('/', (req, res, next) => {
 	});
 });
 
-router.post('/', async (req, res) => {
-	let fields = null;
-	try {
-		fields = lib.helpers.ValidationHelper.validate(req.body, ['email', 'password'], {
-			'email': 'email'
-		}).fields;
-	} catch (ex) {
-		console.log('Invalid fields!');
-		console.log(ex);
-	}
-
-	if (fields !== null) {
-		const user = await req.db.User.findOne({
-			where: {
-				Email: fields.get('email'),
-				IsActive: true
-			}
-		});
-
-		if (user !== null && await user.validPassword(fields.get('password'))) {
-			//successful login
-			req.session.user = user.dataValues;
-			const next = req.query.next ?? 'home';
-			return res.redirect(next);
+router.post('/', validator.body(Joi.object({
+	email: Joi.string()
+		.email()
+		.required(),
+	password: Joi.string()
+		.required()
+})), validator.query(Joi.object({
+	next: Joi.string()
+})), async (req, res, next) => {
+	const user = await req.db.User.findOne({
+		where: {
+			Email: req.body.email,
+			IsActive: true
 		}
+	});
+
+	// either returns if the password is valid, or undefined if no user was found
+	const validLogin = await user?.validPassword(req.body.password);
+
+	if (!validLogin) {
+		return res.render('login', {
+			title: 'Please Log In',
+			error: 'Username or password incorrect. Please try again',
+			email: req.body.email,
+			layout: 'no-nav.hbs'
+		});
 	}
 
-	return res.render('login', {
-		title: 'Please Log In',
-		error: 'Username or password incorrect. Please try again',
-		email: fields.get('email'),
-		layout: 'no-nav.hbs'
-	});
+	req.session.user = user.dataValues;
+	const nextPage = req.query.next ?? 'home';
+	return res.redirect(nextPage);
 });
 
 router.get('/home', (req, res, next) => {
@@ -67,11 +63,10 @@ router.get('/logout', (req, res, next) => {
 	return res.redirect('/');
 });
 
-router.post('/change-band', (req, res, next) => {
-	if (req.body.changeBand == null){
-		return next();
-	}
-
+router.post('/change-band', validator.body(Joi.object({
+	changeBand: Joi.number()
+		.required()
+})), (req, res, next) => {
 	const band = req.session.user.bands.filter(b => b.id === req.body.changeBand);
 
 	if (band.length > 0){
@@ -81,7 +76,9 @@ router.post('/change-band', (req, res, next) => {
 	return res.redirect('back');
 });
 
-router.all('/no-access', (req, res, next) => {
+router.all('/no-access', validator.query(Joi.object({
+	page: Joi.string()
+})), (req, res, next) => {
 	return res.render('no-access.hbs', {
 		title: 'No Access',
 		previousPage: req.query.page ?? ''
