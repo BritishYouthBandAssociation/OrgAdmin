c98e5bec73769a6266d5511e7b603d60ec5eaccb
Luke <rugulous1999@hotmail.com>
2022-08-23 16:07:47 +0100

    Load captions

--- public/js/component/Caption.js
+++ public/js/component/Caption.js
@@ -8,27 +8,27 @@ Vue.component('caption-selector', {
 		<div class="card card-body mb-3">
 			<div class="form-group mb-3">
 				<label :for="'name-' + caption.id">Name</label>
-				<input type="text" class="form-control" :id="'name-' + caption.id" v-model="caption.name" />
+				<input type="text" class="form-control" :id="'name-' + caption.id" v-model="caption.Name" />
 			</div>
 
 			<div class="form-group mb-3">
 				<label :for="'maxScore-' + caption.id">Max Score</label>
-				<input type="number" class="form-control" :id="'maxScore-' + caption.id" v-model="caption.maxScore" v-if="caption.subcaptions.length === 0" />
-				<input type="number" class="form-control" :id="'maxScore-' + caption.id" :value="maxScore" v-if="caption.subcaptions.length > 0" disabled />
+				<input type="number" class="form-control" :id="'maxScore-' + caption.id" v-model="caption.MaxScore" v-if="caption.Subcaptions.length === 0" />
+				<input type="number" class="form-control" :id="'maxScore-' + caption.id" :value="maxScore" v-if="caption.Subcaptions.length > 0" disabled />
 			</div>
 
 			<div class="form-group mb-3">
 				<label :for="'multiplier-' + caption.id">Multiplier</label>
-				<input type="number" class="form-control" :id="'multiplier-' + caption.id" v-model="caption.multiplier" step="0.01"/>
+				<input type="number" class="form-control" :id="'multiplier-' + caption.id" v-model="caption.Multiplier" step="0.01"/>
 			</div>
 
 			<div class="form-group mb-3">
 				<label :for="'optional-' + caption.id">Is Optional?</label>
 				<div class="btn-group" role="group" aria-label="Is Optional?">
-					<input type="radio" value="true" class="btn-check" :name="'optional-' + caption.id" :id="'optional-y-' + caption.id" autocomplete="off" v-model="caption.isOptional">
+					<input type="radio" value="true" class="btn-check" :name="'optional-' + caption.id" :id="'optional-y-' + caption.id" autocomplete="off" v-model="caption.IsOptional">
 					<label class="btn btn-outline-success" :for="'optional-y-' + caption.id">Yes</label>
 
-					<input type="radio" value="false" class="btn-check" :name="'optional-' + caption.id" :id="'optional-n-' + caption.id" autocomplete="off" v-model="caption.isOptional">
+					<input type="radio" value="false" class="btn-check" :name="'optional-' + caption.id" :id="'optional-n-' + caption.id" autocomplete="off" v-model="caption.IsOptional">
 					<label class="btn btn-outline-danger" :for="'optional-n-' + caption.id">No</label>
 				</div>
 			</div>
@@ -36,20 +36,20 @@ Vue.component('caption-selector', {
 			<button class="btn btn-default mb-3" type="button" v-on:click="addCaption">+ Add</button>
 
 			<div class="row row-cols-1 row-cols-lg-2">
-				<caption-selector v-for="c in caption.subcaptions" :key="c.id" :caption="c" />
+				<caption-selector v-for="c in caption.Subcaptions" :key="c.id" :caption="c" />
 			</div>
 		</div>
 	</div>
 	`,
 	methods: {
 		addCaption() {
-			this.caption.subcaptions.push({
+			this.caption.Subcaptions.push({
 				id: --id,
-				name: '',
-				isOptional: false,
-				maxScore: 0,
-				multiplier: 1.0,
-				subcaptions: []
+				Name: '',
+				IsOptional: false,
+				MaxScore: 0,
+				Multiplier: 1.0,
+				Subcaptions: []
 			});
 		}
 	},
@@ -57,13 +57,13 @@ Vue.component('caption-selector', {
 		maxScore: function() {
 			const score = 0;
 
-			if (this.caption.subcaptions.length > 0) {
-				this.caption.maxScore = this.caption.subcaptions.reduce((acc, cap) => {
-					return acc + parseInt(cap.maxScore);
+			if (this.caption.Subcaptions.length > 0) {
+				this.caption.MaxScore = this.caption.Subcaptions.reduce((acc, cap) => {
+					return acc + parseInt(cap.MaxScore);
 				}, 0);
 			}
 
-			return this.caption.maxScore;
+			return this.caption.MaxScore;
 		}
 	}
 });
