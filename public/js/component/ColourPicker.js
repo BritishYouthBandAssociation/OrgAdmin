e7f78eaee00946219fc836fad08154010bb4c466
Luke <rugulous1999@hotmail.com>
2022-09-08 21:50:53 +0100

    Colour picker doesn't bind to props anymore

--- public/js/component/ColourPicker.js
+++ public/js/component/ColourPicker.js
@@ -12,14 +12,28 @@ Vue.component('colour-picker', {
 			<div class="row">
 				<div class="col-9">
 					<div class="input-group">
-						<input type="text" class="form-control" maxlength="20" :id="name" :name="name" v-bind:value="value" v-on:input="$emit('input', $event.target.value)" />
+						<input type="text" class="form-control" maxlength="20" :id="name" :name="name" :value="colour" v-on:input="changeColour($event.target.value)" />
 					</div>
 				</div>
 				<div class="col-3">
-					<input type="color" class="form-control form-control-color" v-bind:value="value" v-on:input="$emit('input', $event.target.value)" />
+					<input type="color" class="form-control form-control-color" :value="colour" v-on:input="changeColour($event.target.value)" />
 				</div>
 			</div>
 		</div>
 	</div>
-	`
+	`,
+	methods: {
+		changeColour(colour) {
+			this.colour = colour;
+			this.$emit('input', colour);
+		}
+	},
+	data: function() {
+		return {
+			colour: ''
+		};
+	},
+	mounted: function() {
+		this.colour = this.value;
+	}
 });
