018ff1a9bb11666b039438dab1c9814d0b0492b2
Luke Taylor <Luke@circleleasing.co.uk>
2022-10-21 12:30:54 +0100

    Upload files to Presto

--- public/js/component/FileUpload.js
+++ public/js/component/FileUpload.js
@@ -2,29 +2,77 @@
 /*global Vue*/
 
 Vue.component('file-upload', {
-	props: ['src', 'title', 'name', 'accept', 'instructions'],
+	props: ['src', 'title', 'name', 'accept', 'instructions', 'token'],
 	template: `
 	<div class="card mb-3">
 		<div class="card-header">
 			<label :for="name">{{title}}</label>
 		</div>
 		<div class="card-body">
-			<img :src="url" class="img-fluid" />
-			<input type="file" :name="name" :accept="fileType" class="form-control" v-on:change="changeFile" />
+			<img :src="url" class="img-fluid mb-3" />
+			<input type="file" :accept="fileType" class="form-control" v-on:change="changeFile" />
 			<p v-html="instructions" v-if="instructions != null && instructions.length > 0"></p>
+
+			<div class="mt-3">
+				<div class="progress" v-if="progress > -1">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" :aria-valuenow="progress" aria-valuemin="0" aria-valuemax="100" :style="{width: progress + '%'}"></div>
+				</div>
+			
+			</div>
+			
+			<input type="hidden" :value="id" :name="name" />
 		</div>
 	</div>
 	`,
 	methods: {
-		changeFile(e){
+		changeFile(e) {
+			if (!e.target.files.length) {
+				this.$emit('cancel');
+				this.url = this.src;
+				return;
+			}
+
 			const file = e.target.files[0];
 			this.url = URL.createObjectURL(file);
+
+			const data = new FormData();
+			data.append('file', file);
+
+			const xhr = new XMLHttpRequest();
+			xhr.upload.onprogress = (e) => {
+				this.progress = e.loaded / e.total * 100;
+			};
+
+			xhr.upload.onerror = (e) => {
+				console.log(e);
+			};
+
+			xhr.upload.onload = (e) => {
+				this.progress = 100;
+			};
+
+			xhr.onreadystatechange = () => {
+				if (xhr.readyState === xhr.DONE) {
+					const json = JSON.parse(xhr.responseText);
+					this.id = json.Value.id;
+					this.$emit('complete', this.id);
+				}
+			};
+
+			xhr.open('POST', `${window.uploadServer}upload/new`);
+			xhr.setRequestHeader('Authorization', `Bearer ${this.token}`);
+			xhr.send(data);
+
+			this.progress = 0;
+			this.$emit('start');
 		}
 	},
 	data: function() {
 		return {
 			url: '',
-			fileType: ''
+			fileType: '',
+			progress: -1,
+			id: null
 		};
 	},
 	mounted: function() {
