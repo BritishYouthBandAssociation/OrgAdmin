01a3d25bda7d7d2563b47443646a4fa11380d262
Luke Taylor <Luke@circleleasing.co.uk>
2023-01-24 12:35:23 +0000

    Sort season order and auto-showing

--- routes/config.js
+++ routes/config.js
@@ -449,7 +449,10 @@ router.get('/season', checkAdmin, validator.query(Joi.object({
 			id: {
 				[Op.not]: season?.id ?? 0
 			}
-		}
+		},
+		order: [
+			['Start', 'DESC']
+		]
 	});
 
 	return res.render('config/season.hbs', {
