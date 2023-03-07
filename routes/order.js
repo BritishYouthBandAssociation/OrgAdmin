e58a549a5fcffbfd74bae0469198292f80a5ed13
matt <matt@fiddaman.net>
2023-03-07 01:11:28 +0000

    Add orders page

---
+++ routes/order.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Import modules
+const express = require('express');
+const Joi = require('joi');
+
+const validator = require('@byba/express-validator');
+const { checkAdmin } = require('../middleware');
+
+const {
+	helpers: {
+		SortHelper
+	}
+} = require(global.__lib);
+
+const router = express.Router();
+
+const idParamSchema = Joi.object({
+	id: Joi.string()
+		.guid()
+		.required()
+});
+
+router.get('/', checkAdmin, validator.query(Joi.object({
+	status: Joi.string().optional()
+})), async (req, res, next) => {
+	const orderWhere = {};
+	if (req.query.status && req.query.status !== 'all') {
+		orderWhere.Status = req.query.status;
+	}
+
+	const orders = (await req.db.Order.findAll({ where: orderWhere })).sort((a, b) => SortHelper.dateProp('Date', a, b, false));
+	const orderStatuses = req.db.Order.getAttributes().Status.values;
+
+	return res.render('order/index.hbs', {
+		title: 'Orders',
+		orders,
+		orderStatuses,
+		currentStatus: req.query.status ?? 'all'
+	});
+});
+
+module.exports = {
+	root: '/order/',
+	router: router
+};
